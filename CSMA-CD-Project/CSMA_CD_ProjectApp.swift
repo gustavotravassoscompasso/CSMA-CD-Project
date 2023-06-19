59bfdad5730a16bc10c27068fca874ac158ad125
Gustavo Travassos <92793355+gustavotravassoscompasso@users.noreply.github.com>
2023-06-19 20:22:15 -0300

    Initial Commit

---
+++ CSMA-CD-Project/CSMA_CD_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSMA_CD_ProjectApp.swift
+//  CSMA-CD-Project
+//
+//  Created by Gustavo Igor Goncalves Travassos on 19/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CSMA_CD_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
